efe5447923747a1880ea474a5f5cbbb502476af8
Andrei Rechkov <andrei.rechkov@gmail.com>
2025-02-16 18:06:55 +0100

    github-book: create a func hello (2 step from TDD)

--- main.go
+++ main.go
@@ -1,35 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-const epsilon = 1e-9
-
-func areFloatsEqual(a, b float64) bool {
-    return math.Abs(a - b) < epsilon
-}
-
-
-func Sqrt(x float64) float64 {
-	z := x / 2
-	prev_value := z
-
-	for i := 0; i < 10; i++ {
-		z -= (z*z - x) / (2 * z)
-		
-		if areFloatsEqual(prev_value, z) {
-			break
-		}
-		
-		fmt.Printf("Iter %d: z = %f\n", i+1, z)
-		prev_value = z
-	}
-
-	return z
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Hello("Andrey"))
 }
