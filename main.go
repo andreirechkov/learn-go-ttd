736282df3bc4da9ebb39825a8ecf79248143a7c5
Andrei Rechkov <andrei.rechkov@gmail.com>
2025-02-16 17:39:18 +0100

    main: testing book

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func swap(x, y string) (string, string) {
-  return y, x
+const epsilon = 1e-9
+
+func areFloatsEqual(a, b float64) bool {
+    return math.Abs(a - b) < epsilon
 }
 
 
+func Sqrt(x float64) float64 {
+	z := x / 2
+	prev_value := z
+
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		
+		if areFloatsEqual(prev_value, z) {
+			break
+		}
+		
+		fmt.Printf("Iter %d: z = %f\n", i+1, z)
+		prev_value = z
+	}
+
+	return z
+}
+
 func main() {
-  a, b := swap("hello", "world")
-  fmt.Println(a, b)
+	fmt.Println(Sqrt(2))
 }
